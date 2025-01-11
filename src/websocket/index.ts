4624f72a00ed86172aeb4fee69723b34e6c8867b
lukaliuzzi <lukaliuzzidev@gmail.com>
2025-01-11 17:29:44 -0300

    update WebSocket path for socket.io

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -17,7 +17,7 @@ const setupWebSocket = (server: HttpServer) => {
       methods: ["GET", "POST"],
       credentials: true,
     },
-    path: "/multiplayer-quiz-back/socket.io",
+    path: "/socket.io",
     connectionStateRecovery: {
       maxDisconnectionDuration: 15000,
     },
